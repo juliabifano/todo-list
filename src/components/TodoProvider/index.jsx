bb6178deda40f3a04fcc0896b6e4000c6514e59d
Júlia Bifano <juliabifanovieira@gmail.com>
2026-02-13 12:58:43 -0300

    Adiciona itens à local storage

--- src/components/TodoProvider/index.jsx
+++ src/components/TodoProvider/index.jsx
@@ -1,21 +1,16 @@
 import TodoContext from "./TodoContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const TODOS = "todos";
 
 export function TodoProvider({ children }) {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      description: "JSX e componentes",
-      completed: false,
-      createdAt: "2022-10-31",
-    },
-    {
-      id: 2,
-      description: "Controle de inputs e formulários controlados",
-      completed: true,
-      createdAt: "2022-10-31",
-    },
-  ]);
+  const savedTodos = localStorage.getItem(TODOS);
+
+  const [todos, setTodos] = useState(savedTodos ? JSON.parse(savedTodos) : []);
+
+  useEffect(() => {
+    localStorage.setItem(TODOS, JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (formData) => {
     const description = formData.get("description");
@@ -50,7 +45,9 @@ export function TodoProvider({ children }) {
     });
   };
 
-  return <TodoContext value={{todos, addTodo, toggleTodoCompleted, deleteTodo}}>{children}</TodoContext>;
+  return (
+    <TodoContext value={{ todos, addTodo, toggleTodoCompleted, deleteTodo }}>
+      {children}
+    </TodoContext>
+  );
 }
-
-
